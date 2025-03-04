028a60a104609cef6ea4fdad6c4f23cd9f352818
oneny <gjwodnjs1996@gmail.com>
2025-03-05 05:56:03 +0900

    feat: ApiResponse 공통 응답 객체 생성

---
+++ idolu-product/src/main/java/com/idolu/product/global/common/ApiResponse.java
@@ -0,0 +1,35 @@
+package com.idolu.product.global.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * 공통적으로 사용하는 응답 포맷
+ */
+@Getter
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, String message, T data) {
+        return new ApiResponse<>(httpStatus, message, data);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        return of(httpStatus, httpStatus.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return of(HttpStatus.OK, data);
+    }
+}
