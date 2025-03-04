3d1989642cac420109be5fbacb8f5df05aef4607
oneny <gjwodnjs1996@gmail.com>
2025-03-05 06:11:35 +0900

    feat: Product 도메인 객체 생성

---
+++ idolu-product/src/main/java/com/idolu/product/domain/product/Product.java
@@ -0,0 +1,40 @@
+package com.idolu.product.domain.product;
+
+
+import com.idolu.product.global.common.BaseEntity;
+import lombok.AccessLevel;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+
+@Getter
+@Table("product")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class Product extends BaseEntity {
+
+    @Id
+    private Long id;
+
+    private Integer stock;
+
+    private String name;
+
+    private String imageUrl;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private ProductStatus status;
+
+    private Boolean isDeleted;
+
+    @Version
+    private Integer version;
+}
---
+++ idolu-product/src/main/java/com/idolu/product/domain/product/ProductStatus.java
@@ -0,0 +1,9 @@
+package com.idolu.product.domain.product;
+
+public enum ProductStatus {
+    COMING_SOON,
+    ON_SALE,
+    SOLD_OUT,
+    TERMINATED
+    ;
+}
