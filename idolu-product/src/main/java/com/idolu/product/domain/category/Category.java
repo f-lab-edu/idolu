fc085a62ed64fd00895f5047de1d19046293b9d0
oneny <gjwodnjs1996@gmail.com>
2025-03-06 21:00:39 +0900

    feat: 글로벌 에러 핸들링 구현

---
+++ idolu-product/src/main/java/com/idolu/product/domain/category/Category.java
@@ -0,0 +1,8 @@
+package com.idolu.product.domain.category;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table("category")
+public class Category {
+
+}
---
+++ idolu-product/src/main/java/com/idolu/product/global/advice/ExceptionAdvice.java
@@ -0,0 +1,33 @@
+package com.idolu.product.global.advice;
+
+import com.idolu.product.global.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected Mono<ApiResponse<String>> illegalArgumentException(IllegalArgumentException exception) {
+        log.warn("IllegalArgumentException: {}", exception.getMessage());
+        return Mono.just(ApiResponse.of(HttpStatus.BAD_REQUEST, exception.getMessage(), null)) ;
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    protected Mono<ApiResponse<String>> bindException(WebExchangeBindException exception) {
+        log.warn("WebExchangeBindException: {}", exception.getMessage());
+        return Mono.just(ApiResponse.of(
+                HttpStatus.BAD_REQUEST,
+                exception.getBindingResult().getAllErrors().get(0).getDefaultMessage(),
+                null
+        ));
+    }
+}
