d274795842af82138fc22b29b0ae7346dac30b6f
oneny <gjwodnjs1996@gmail.com>
2025-03-10 23:03:34 +0900

    feat: 카테고리 생성 로그 추가

--- idolu-product/src/main/java/com/idolu/product/presentation/product/ProductController.java
+++ idolu-product/src/main/java/com/idolu/product/presentation/product/ProductController.java
@@ -6,10 +6,12 @@ import com.idolu.product.presentation.product.request.ProductCreateRequest;
 import com.idolu.product.presentation.product.response.ProductCreateResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
@@ -21,6 +23,7 @@ public class ProductController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<ApiResponse<ProductCreateResponse>> createProduct(@Valid @RequestBody ProductCreateRequest request) {
         return productService.createProduct(request.toCommand())
+                .doOnNext(id -> log.info("id {} Product 생성", id))
                 .map(id -> ApiResponse.of(HttpStatus.CREATED, ProductCreateResponse.from(id)));
     }
 }
