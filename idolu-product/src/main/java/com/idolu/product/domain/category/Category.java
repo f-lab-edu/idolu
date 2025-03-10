a5a50e521bec669f0eda563f7ecc09ec2c91270d
oneny <gjwodnjs1996@gmail.com>
2025-03-10 23:04:52 +0900

    feat: 카테고리 Entity 생성

--- idolu-product/src/main/java/com/idolu/product/domain/category/Category.java
+++ idolu-product/src/main/java/com/idolu/product/domain/category/Category.java
@@ -1,8 +1,26 @@
 package com.idolu.product.domain.category;
 
+import com.idolu.product.global.common.BaseEntity;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+@Getter
+@Builder
 @Table("category")
-public class Category {
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = false)
+public class Category extends BaseEntity {
 
+    @Id
+    @Column("category_id")
+    private Long id;
+
+    private String categoryCode;
+
+    private String categoryName;
+
+    private Boolean isDeleted;
 }
