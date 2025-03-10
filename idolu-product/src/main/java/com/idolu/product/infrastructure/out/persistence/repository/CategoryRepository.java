c47a101e29f59fc55ece3fdc353fce1ae353b77a
oneny <gjwodnjs1996@gmail.com>
2025-03-10 23:07:39 +0900

    feat: CategoryAdapter 생성 및 카테고리 확인 로직 구현

---
+++ idolu-product/src/main/java/com/idolu/product/infrastructure/out/persistence/repository/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.idolu.product.infrastructure.out.persistence.repository;
+
+import com.idolu.product.domain.category.Category;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+public interface CategoryRepository extends R2dbcRepository<Category, Long> {
+
+    Flux<Category> findByCategoryCodeIn(List<String> categoryCodes);
+}
