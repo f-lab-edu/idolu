31773ac47baab279790b7a637ccf20faec9c44e1
oneny <gjwodnjs1996@gmail.com>
2025-03-11 19:27:33 +0900

    feat: 상품 및 상품 카테고리 저장 Adapter 수정

--- idolu-product/src/main/java/com/idolu/product/domain/productcategory/ProductCategory.java
+++ idolu-product/src/main/java/com/idolu/product/domain/productcategory/ProductCategory.java
@@ -1,10 +1,14 @@
 package com.idolu.product.domain.productcategory;
 
+import com.idolu.product.domain.product.Product;
 import com.idolu.product.global.common.BaseEntity;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
@@ -23,4 +27,14 @@ public class ProductCategory extends BaseEntity {
     private Long categoryId;
 
     private Boolean isDeleted;
+
+    public static List<ProductCategory> from(Product product) {
+        return product.getCategories().stream()
+                .map(category -> ProductCategory.builder()
+                        .productId(product.getId())
+                        .categoryId(category.getId())
+                        .isDeleted(category.getIsDeleted() != null && category.getIsDeleted())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
