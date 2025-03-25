610c7de9e6fb648982035a4e2b6fdd4ab3430bbc
oneny <gjwodnjs1996@gmail.com>
2025-03-25 20:53:22 +0900

    feat: ProductCategory 조회 및 삭제 adapter 구현

--- idolu-product/src/main/java/com/idolu/product/infrastructure/out/persistence/repository/ProductCategoryRepository.java
+++ idolu-product/src/main/java/com/idolu/product/infrastructure/out/persistence/repository/ProductCategoryRepository.java
@@ -2,6 +2,12 @@ package com.idolu.product.infrastructure.out.persistence.repository;
 
 import com.idolu.product.domain.productcategory.ProductCategory;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ProductCategoryRepository extends R2dbcRepository<ProductCategory, Long> {
+
+    Flux<ProductCategory> findByProductId(Long productId);
+
+    Mono<Void> deleteByCategoryIdAndProductId(Long categoryId, Long productId);
 }
