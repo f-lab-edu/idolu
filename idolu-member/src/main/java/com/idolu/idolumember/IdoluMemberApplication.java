b18b3dbbe462ec399f4861954050b29c0e214489
oneny <gjwodnjs1996@gmail.com>
2025-05-03 20:56:52 +0900

    chore: member 프로젝트 생성

---
+++ idolu-member/src/main/java/com/idolu/idolumember/IdoluMemberApplication.java
@@ -0,0 +1,13 @@
+package com.idolu.idolumember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdoluMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdoluMemberApplication.class, args);
+    }
+
+}
