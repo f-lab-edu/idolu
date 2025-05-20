439b4d1a48fb8c8ff33cbcaf72e5182004f1a42d
oneny <gjwodnjs1996@gmail.com>
2025-05-20 18:39:17 +0900

    chore: 주문 프로젝트 생성

---
+++ idolu-order/src/test/java/com/idolu/idoluorder/IdoluOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idolu.idoluorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdoluOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
