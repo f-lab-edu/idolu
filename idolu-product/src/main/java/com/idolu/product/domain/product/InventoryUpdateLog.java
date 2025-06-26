86799b56bd587a3e00cf2cdd4b30d43780e47c3d
oneny <gjwodnjs1996@gmail.com>
2025-06-26 19:36:54 +0900

    feat: InventoryUpdateLog entity 수정

--- idolu-product/src/main/java/com/idolu/product/domain/product/InventoryUpdateLog.java
+++ idolu-product/src/main/java/com/idolu/product/domain/product/InventoryUpdateLog.java
@@ -1,10 +1,12 @@
 package com.idolu.product.domain.product;
 
-import com.idolu.product.domain.product.type.StockUpdateType;
+import com.idolu.product.domain.product.type.StockType;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -21,10 +23,21 @@ public class InventoryUpdateLog {
 
     private String orderNo;
 
-    private Integer stock;
+    private Integer quantity;
 
-    private StockUpdateType type;
+    private StockType type;
 
     @CreatedDate
     private LocalDateTime createdAt;
+
+    @Builder
+    @PersistenceCreator
+    public InventoryUpdateLog(Long inventoryUpdateLogId, Long productId, String orderNo, Integer quantity, StockType type, LocalDateTime createdAt) {
+        this.inventoryUpdateLogId = inventoryUpdateLogId;
+        this.productId = productId;
+        this.orderNo = orderNo;
+        this.quantity = quantity;
+        this.type = type;
+        this.createdAt = createdAt;
+    }
 }
--- idolu-product/src/main/java/com/idolu/product/domain/product/type/StockUpdateType.java
+++
@@ -1,6 +0,0 @@
-package com.idolu.product.domain.product.type;
-
-public enum StockUpdateType {
-    INCREASE,
-    DECREASE
-}
