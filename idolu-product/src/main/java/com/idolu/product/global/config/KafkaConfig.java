e6789e81e35110ed02072b4ffa9d7ab98cc0d3a9
oneny <gjwodnjs1996@gmail.com>
2025-06-26 19:54:16 +0900

    chore: 카프카 Producer 설정

--- idolu-product/src/main/java/com/idolu/product/global/config/KafkaConfig.java
+++ idolu-product/src/main/java/com/idolu/product/global/config/KafkaConfig.java
@@ -1,13 +1,17 @@
 package com.idolu.product.global.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
 import reactor.kafka.receiver.ReceiverOptions;
+import reactor.kafka.sender.SenderOptions;
 
 import java.util.Collections;
 import java.util.Map;
@@ -37,5 +41,18 @@ public class KafkaConfig {
         ReceiverOptions<String, String> receiverOptions = ReceiverOptions.<String, String>create(props)
                 .subscription(Collections.singletonList(orderStockRollbackTopic));
         return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
+    }
+
+    @Bean
+    public ReactiveKafkaProducerTemplate<String, String> kafkaProducerTemplate(KafkaProperties kafkaProperties) {
+        Map<String, Object> props = kafkaProperties.buildProducerProperties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.ACKS_CONFIG, "-1");
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+
+        SenderOptions<String, String> senderOptions = SenderOptions.create(props);
+        return new ReactiveKafkaProducerTemplate<>(senderOptions);
     }
 }
